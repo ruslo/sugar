3c254e0d56c72c25d76c7412f707cf344501e152
ruslo <ruslan_baratov@yahoo.com>
2013-10-19 20:37:03 +0400

    Update sugar_fatal_error.cmake

--- cmake/print/sugar_fatal_error.cmake
+++ cmake/print/sugar_fatal_error.cmake
@@ -6,7 +6,7 @@ sugar_add_this_to_sourcelist()
 
 # Warning:
 #     do not include/use 'sugar_improper_number_of_arguments',
-#     because it use 'sugar_fatal_error' macro
+#     because it use 'sugar_fatal_error' function
 
 function(sugar_fatal_error)
   if(${ARGC} EQUAL 0)
