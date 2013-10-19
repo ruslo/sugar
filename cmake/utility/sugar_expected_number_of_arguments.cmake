5744655fd56fd0a599ac5591791e8bf44f516c0d
ruslo <ruslan_baratov@yahoo.com>
2013-10-19 21:28:34 +0400

    Update sugar_expected_number_of_arguments.cmake

--- cmake/utility/sugar_expected_number_of_arguments.cmake
+++ cmake/utility/sugar_expected_number_of_arguments.cmake
@@ -6,18 +6,6 @@ sugar_add_this_to_sourcelist()
 
 include(sugar_fatal_error)
 
-# By default number of arguments passed to a function not controlled.
-# i.e. |function(my_function A B)| can be called with 3 arguments.
-# This function help to check this.
-#
-# Example:
-#     function(my_function A B)
-#       sugar_expected_number_of_arguments(${ARGC} 2)
-#       # ...
-#     endfunction()
-#
-#     my_function(1 2) # OK
-#     my_function(1 2 3) # ERROR
 function(sugar_expected_number_of_arguments given expected)
   # Do some self check.
   if(NOT ${ARGC} EQUAL 2)
