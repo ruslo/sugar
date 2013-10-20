24bff9ba70adbac95a56b6fd8cfe30134902840f
Ruslan Baratov <ruslan_baratov@yahoo.com>
2013-10-20 16:19:08 +0400

    Rename internal

--- cmake/core/sugar_setup_gitenv_paths.cmake
+++ cmake/core/sugar_setup_gitenv_paths.cmake
@@ -7,18 +7,20 @@ sugar_add_this_to_sourcelist()
 include(sugar_expected_number_of_arguments)
 include(sugar_test_variable_not_empty)
 
-macro(sugar_setup_libraries_paths)
+macro(sugar_setup_gitenv_paths)
   sugar_expected_number_of_arguments(${ARGC} 0)
-  if(DEFINED GITENV_ROOT)
-    sugar_test_variable_not_empty(GITENV_ROOT)
-
-    set(Boost_NO_SYSTEM_PATH TRUE) # disable searching in system dirs
-    set(BOOST_ROOT "${GITENV_ROOT}/boost/install")
-
-    set(GMOCK_ROOT "${GITENV_ROOT}/google/gmock")
-    set(GTEST_ROOT "${GITENV_ROOT}/google/gtest")
-    set(BREAKPAD_ROOT "${GITENV_ROOT}/google/breakpad")
-    set(SAFE_NUMERICS_ROOT "${GITENV_ROOT}/safe_numerics")
-    set(SOBER_ROOT "${GITENV_ROOT}/ruslo/sober")
+  if(NOT DEFINED GITENV_ROOT)
+    set(GITENV_ROOT "$ENV{GITENV_ROOT}")
   endif()
+
+  sugar_test_variable_not_empty(GITENV_ROOT)
+
+  set(Boost_NO_SYSTEM_PATH TRUE) # disable searching in system dirs
+  set(BOOST_ROOT "${GITENV_ROOT}/boost/install")
+
+  set(GMOCK_ROOT "${GITENV_ROOT}/google/gmock")
+  set(GTEST_ROOT "${GITENV_ROOT}/google/gtest")
+  set(BREAKPAD_ROOT "${GITENV_ROOT}/google/breakpad")
+  set(SAFE_NUMERICS_ROOT "${GITENV_ROOT}/safe_numerics")
+  set(SOBER_ROOT "${GITENV_ROOT}/ruslo/sober")
 endmacro()
