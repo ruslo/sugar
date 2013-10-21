e1ddf582244c1b489a1a25151ef3860637360d6e
Ruslan Baratov <ruslan_baratov@yahoo.com>
2013-10-22 00:01:05 +0400

    Add example: doxygen

---
+++ cmake/utility/sugar_add_definitions_debug.cmake
@@ -0,0 +1,18 @@
+# Copyright (c) 2013, Ruslan Baratov
+# All rights reserved.
+
+include(sugar_add_this_to_sourcelist)
+sugar_add_this_to_sourcelist()
+
+include(sugar_expected_number_of_arguments)
+
+macro(sugar_add_definitions_debug definition)
+  sugar_expected_number_of_arguments(${ARGC} 1)
+  set_property(
+      DIRECTORY
+      APPEND
+      PROPERTY
+      COMPILE_DEFINITIONS_DEBUG
+      "${definition}"
+  )
+endmacro()
