e03b4de27d484d2b9e681ad3614ee841aba31efa
Ruslan Baratov <ruslan_baratov@yahoo.com>
2013-11-06 21:51:29 +0400

    Add sugar_add_gtest.cmake

---
+++ cmake/core/sugar_add_gtest.cmake
@@ -0,0 +1,26 @@
+# Copyright (c) 2013, Ruslan Baratov
+# All rights reserved.
+
+include(sugar_add_this_to_sourcelist)
+sugar_add_this_to_sourcelist()
+
+include(sugar_add_ios_gtest)
+include(sugar_status_debug)
+
+function(sugar_add_gtest)
+  set(iphoneos_found FALSE)
+  foreach(x ${CMAKE_OSX_SYSROOT})
+    string(FIND "${x}" "iphoneos" result)
+    if(NOT ${result} EQUAL "-1")
+      set(iphoneos_found TRUE)
+    endif()
+  endforeach()
+
+  if(iphoneos_found)
+    sugar_status_debug("Use sugar_add_ios_gtest")
+    sugar_add_ios_gtest(${ARGV})
+  else()
+    sugar_status_debug("Use cmake add_test")
+    add_test(${ARGV})
+  endif()
+endfunction()
