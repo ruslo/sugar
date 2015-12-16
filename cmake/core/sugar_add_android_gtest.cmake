f02313d0263b24ccf9eb0ae06adb6fb62a45113d
t.zemaitis <t.zemaitis@idscan.co.uk>
2015-12-16 09:17:11 +0200

    Added general function which lets add tests on all platforms

---
+++ cmake/core/sugar_add_android_gtest.cmake
@@ -0,0 +1,53 @@
+# Copyright (c) 2013, Ruslan Baratov
+# All rights reserved.
+
+include(CMakeParseArguments) # cmake_parse_arguments
+
+include(sugar_add_this_to_sourcelist)
+sugar_add_this_to_sourcelist()
+
+set(ANDROID_THIS_DIRECTORY "${CMAKE_CURRENT_LIST_DIR}")
+
+function(sugar_add_android_gtest)
+  set(ADB_COMMAND "adb")
+
+  cmake_parse_arguments(x "" "NAME" "COMMAND" ${ARGV})
+  string(COMPARE NOTEQUAL "${x_UNPARSED_ARGUMENTS}" "" has_unparsed)
+  if(has_unparsed)
+    message(FATAL_ERROR "Unparsed: ${x_UNPARSED_ARGUMENTS}")
+  endif()
+
+  list(GET x_COMMAND 0 app_target)
+  if(NOT TARGET "${app_target}")
+    message(FATAL_ERROR "Expected executable target as first argument, but got: ${app_target}")
+  endif()
+
+  set(script_loc "${CMAKE_CURRENT_BINARY_DIR}/_3rdParty/AndroidTest/${x_NAME}.cmake")
+
+  list(REMOVE_AT x_COMMAND 0)
+  set(APP_ARGUMENTS ${x_COMMAND})
+
+  set(ANDROID_APK_APP_DESTINATION "/data/local/tmp/AndroidApk")
+  set(APP_DESTINATION "${ANDROID_APK_APP_DESTINATION}")
+  set(APP_DESTINATION "${APP_DESTINATION}/${PROJECT_NAME}/AndroidTest")
+  set(APP_DESTINATION "${APP_DESTINATION}/${x_NAME}/${app_target}")
+
+  # Use:
+  # * ADB_COMMAND
+  # * APP_ARGUMENTS
+  # * APP_DESTINATION
+  configure_file(
+      "${ANDROID_THIS_DIRECTORY}/templates/AndroidTest.cmake.in"
+      "${script_loc}"
+      @ONLY
+  )
+
+  add_test(
+      NAME "${x_NAME}"
+      COMMAND
+          "${CMAKE_COMMAND}"
+          "-DAPP_SOURCE=$<TARGET_FILE:${app_target}>"
+          -P
+          "${script_loc}"
+  )
+endfunction()
--- cmake/core/sugar_add_gtest.cmake
+++ cmake/core/sugar_add_gtest.cmake
@@ -1,20 +1,36 @@
 # Copyright (c) 2013, Ruslan Baratov
 # All rights reserved.
 
+include(CMakeParseArguments) # cmake_parse_arguments
+
 include(sugar_add_this_to_sourcelist)
 sugar_add_this_to_sourcelist()
 
 include(sugar_add_ios_gtest)
+include(sugar_add_android_gtest)
 include(sugar_status_debug)
+
+if(HUNTER_ENABLED)
+  string(COMPARE EQUAL "${CMAKE_OSX_SYSROOT}" "iphoneos" is_ios)
+  if(is_ios)
+    hunter_add_package(ios_sim)
+  endif()
+endif()
 
 function(sugar_add_gtest)
   string(COMPARE EQUAL "${CMAKE_OSX_SYSROOT}" "iphoneos" is_ios)
   if(is_ios)
     sugar_status_debug("Use sugar_add_ios_gtest")
     sugar_status_debug("ARGV: [${ARGV}]")
-    sugar_add_ios_gtest(${ARGV})
+    cmake_parse_arguments(x "" "NAME" "COMMAND" ${ARGV})
+    sugar_add_ios_gtest(${x_NAME} ${x_COMMAND})
+  elseif(ANDROID)
+    sugar_status_debug("Use android_add_test")
+    sugar_status_debug("ARGV: [${ARGV}]")
+    sugar_add_android_gtest(${ARGV})
   else()
     sugar_status_debug("Use cmake add_test")
+    sugar_status_debug("ARGV: [${ARGV}]")
     add_test(${ARGV})
   endif()
 endfunction()
